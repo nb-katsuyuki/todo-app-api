cbc8a5ca066eb003449309d4fd094560ae2a547c
katsuyuki suzuki <katsuyuki.suzuki@nextbeat.net>
2021-11-15 11:37:27 +0900

    ready

--- app/controllers/Home.scala
+++ app/controllers/Home.scala
@@ -1,7 +1,5 @@
 /**
- *
  * to do sample project
- *
  */
 
 package controllers
@@ -12,13 +10,14 @@ import play.api.mvc._
 import model.ViewValueHome
 
 @Singleton
-class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class HomeController @Inject() (val controllerComponents: ControllerComponents)
+    extends BaseController {
 
   def index() = Action { implicit req =>
     val vv = ViewValueHome(
-      title  = "Home",
+      title = "Home",
       cssSrc = Seq("main.css"),
-      jsSrc  = Seq("main.js")
+      jsSrc = Seq("main.js")
     )
     Ok(views.html.Home(vv))
   }
